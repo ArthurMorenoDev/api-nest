0fb127982d1e4ea29b884067a32ed1f5e468c830
Arthyr-Moreno <arthurmoreno31@gmail.com>
2024-08-14 12:36:33 -0300

    primeiro comit

---
+++ src/users/user.controller.ts
@@ -0,0 +1,41 @@
+import{Body, Controller, Get, Param, Post, Query} from "@nestjs/common";
+import { randomUUID } from "crypto";
+
+type ParamsUser = {
+    id:string,
+    idEmpresa: string,
+}
+
+type QueryUser = {
+    p:string,
+    r:string
+}
+
+type BodyUser ={
+    name:string,
+    age:string
+}
+@Controller("/users")
+export class UserController {
+
+    @Get("/:id/:idEmpresa")
+    findById(@Param() params: ParamsUser){
+        return "Usuario do ID " + params.id + " - Empresa ID "+params.idEmpresa;
+    }
+    
+    @Get("/findByPages")
+    findByPages(@Query() queries:QueryUser){
+        return "Queries" + queries.p;
+    }
+
+
+    @Post("/create")
+    create(@Body() data:BodyUser){
+        return {
+            ...data,
+            id:randomUUID(),
+        }
+    }
+
+
+}
